b04ea8e28d5bde7d849945db3e968d8117b5cf44
fredrcp <fredrcpmed@gmail.com>
2026-09-22 09:27:06 -0300

    fix: hydration error na splash screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,11 +9,11 @@ export const metadata: Metadata = {
   manifest: '/manifest.json',
   icons: {
     icon: [
-      { url: '/favicon.ico', sizes: '32x32', type: 'image/x-icon' },
-      { url: '/favicon-32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon.ico?v=3', sizes: '32x32', type: 'image/x-icon' },
+      { url: '/favicon-32.png?v=3', sizes: '32x32', type: 'image/png' },
     ],
     apple: [
-      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+      { url: '/apple-touch-icon.png?v=3', sizes: '180x180', type: 'image/png' },
     ],
   },
 }
